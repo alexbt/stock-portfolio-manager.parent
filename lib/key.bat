acc128d5a72fde5c75e351618329358aeb128ed3
alex.belisleturcot <alex.belisleturcot@localhost>
2011-07-21 23:57:06 -0400

    Remove unused librairies

--- lib/key.bat
+++
@@ -1,5 +0,0 @@
-set path=%path%;C:\Program Files\Java\jdk1.6.0_13\bin
-
-keytool -genkey -alias proserus -keystore portfolio.cert < keystore.conf
-
-pause
--- lib/sign.bat
+++
@@ -1,9 +0,0 @@
-set path=%path%;C:\Program Files\Java\jdk1.6.0_13\bin
-
-#jarsigner -keystore portfolio.cert -storepass password -keypass password Portfolio.jar proserus
-jarsigner -keystore portfolio.cert -storepass password -keypass password joda-time-1.6_part2.jar proserus
-jarsigner -keystore portfolio.cert -storepass password -keypass password joda-time-1.6_part1.jar proserus
-#jarsigner -keystore portfolio.cert -storepass password -keypass password jcommon-1.0.16.jar proserus
-#jarsigner -keystore portfolio.cert -storepass password -keypass password jfreechart-1.0.13.jar proserus
-
-pause
