b895df6e0c458d68056b5df0d589b1fcbd7b100a
Alex <Alex@localhost>
2014-04-03 23:29:04 -0400

    - Mavenize projects - Externalize sectors in a property files - Fix nullpointer when displaying AddTransaction pannel - Add a catchall throwable from swing and log - Fix symbols table not being refreshed in some cases

--- launch.bat
+++ launch.bat
@@ -3,15 +3,15 @@ echo off
 set logfile=launch.log
 echo -- >> %logfile%
 echo %date%, %time% >> %logfile%
-echo set classpath=.;StockPortfolioLauncher.jar >> %logfile%
+echo set classpath=.;stockportfolioswing-0.24.jar >> %logfile%
 
-echo java -jar StockPortfolioLauncher.jar >> %logfile%
+echo java -jar stockportfolioswing-0.24.jar >> %logfile%
 
 
 echo on
-set classpath=.;StockPortfolioLauncher.jar 2>> %logfile%
+set classpath=.;stockportfolioswing-0.24.jar 2>> %logfile%
 
-java -jar StockPortfolioLauncher.jar 2>> %logfile%
+java -jar stockportfolioswing-0.24.jar 2>> %logfile%
 
 
 pause
