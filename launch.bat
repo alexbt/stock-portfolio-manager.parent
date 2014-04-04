232a7847a31c58131a912bf3af832cea88965d6a
alex belisleturcot <alex.belisleturcot@gmail.com>
2014-04-04 15:36:13 -0400

    update build

---
+++ launch.bat
@@ -0,0 +1,12 @@
+echo on
+
+set logfile=logs/launch.bat.log
+echo %date%.%time% - Launching with launch.bat >> %logfile%
+
+echo %date%.%time% - Previous classpath: %classpath% >> %logfile%
+set classpath=.;StockPortfolioLauncher.jar 2>> %logfile%
+echo %date%.%time% - Current classpath: %classpath% >> %logfile%
+
+java -jar StockPortfolioLauncher.jar 2>> %logfile%
+
+pause
