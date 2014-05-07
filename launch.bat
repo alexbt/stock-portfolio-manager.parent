03557c8a303488b1a1f16c808f7e6b2c0dcd262c
Laptop <Laptop@localhost>
2014-05-07 01:46:52 -0400

    update project name change for javaw on windows change archive folder name

--- launch.bat
+++ launch.bat
@@ -1,12 +1,10 @@
 echo on
 
-set logfile=logs/launch.bat.log
+set logfile=launch.bat.log
 echo %date%.%time% - Launching with launch.bat >> %logfile%
 
 echo %date%.%time% - Previous classpath: %classpath% >> %logfile%
 set classpath=.;StockPortfolioLauncher.jar 2>> %logfile%
 echo %date%.%time% - Current classpath: %classpath% >> %logfile%
 
-java -jar StockPortfolioLauncher.jar 2>> %logfile%
-
-pause
+start javaw -jar StockPortfolioLauncher.jar 2>> %logfile%
